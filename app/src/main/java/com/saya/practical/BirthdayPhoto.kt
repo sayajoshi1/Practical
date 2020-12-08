fdd33c5e1799f48340bb6885442b634a605b31f5
Saya Joshi <joshisaya0@gmail.com>
2020-12-08 18:51:39 +0545

    Initial commit

---
+++ app/src/main/java/com/saya/practical/BirthdayPhoto.kt
@@ -0,0 +1,11 @@
+package com.saya.practical
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BirthdayPhoto : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_birthday_photo)
+    }
+}
